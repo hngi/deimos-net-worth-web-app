a932175da4053307ddb04938506335b796580dbb
Ovie Tennyson <atomictenny@gmail.com>
2019-09-25 00:56:49 -0700

    changes server to app

--- app/Register.php
+++ app/Register.php
@@ -18,8 +18,13 @@ $result = $user->openConnection();
 if($result)
 {   
     $email      = $_POST['email'];
-    $password   = $_POST['password'];
+    $username      = $_POST['username'];
+    $password_1   = $_POST['password_1'];
+    $password_2   = $_POST['password_2'];
+    if($password_1 === $password_2){
+        $user->signup($email,$username,$password_1);
+        $user->closeConnection();
+    }
     
-    $user->signup($email,$password);
-    $user->closeConnection();
+    
 }
