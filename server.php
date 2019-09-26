5bfcdc1c7353907867550427616d3423517ca3c9
Ovie Tennyson <atomictenny@gmail.com>
2019-09-25 23:23:29 -0700

    Validation fixed

--- server.php
+++ server.php
@@ -3,7 +3,8 @@
 	// variable declaration
 	$username = "";
 	$email    = "";
-	$errors = array(); 
+  $errors = array(); 
+  $regError = [];
 	$_SESSION['success'] = "";
 
 	// connect to database
@@ -12,31 +13,62 @@
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
 		// receive all input values from the form
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password = mysqli_real_escape_string($db, $_POST['password']);
-		$confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']);
+		$username         = mysqli_real_escape_string($db, $_POST['username']);
+		$email            = mysqli_real_escape_string($db, $_POST['email']);
+		$password         = mysqli_real_escape_string($db, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']);
 
-		// form validation: ensure that the form is correctly filled
+
+
+    // form validation: ensure that the form is correctly filled
 		if (empty($username)) { array_push($errors, "Username is required"); }
 		if (empty($email)) { array_push($errors, "Email is required"); }
 		if (empty($password)) { array_push($errors, "Password is required"); }
+		if ($password != $confirm_password) {array_push($errors, "The two passwords do not match");}
+		
+    
+    
+      // checks user email
+      $checkEmail   = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+      $checkResult  = mysqli_query($db, $checkEmail);
 
-		if ($password != $confirm_password) {
-			array_push($errors, "The two passwords do not match");
-		}
+      $checkUsername   = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+      $checkUsernameResult  = mysqli_query($db, $checkUsername);
+      
+      if(mysqli_num_rows($checkResult) == 1) 
+      {
+        
+          array_push($errors, "Email already exist, Please try a different email");
+          $_SESSION['error'] = $errors;
+          header('location: login.php');
+      }
+      elseif(mysqli_num_rows($checkUsernameResult) == 1)
+      {
+          array_push($errors, "Username taken, Please try a different username");
+          $_SESSION['error'] = $errors;
+          header('location: login.php');
+      }
+      else 
+      {
+        if (count($errors) == 0) {
+          // register user if there are no errors in the form
+          $password = md5($password);//encrypt the password before saving in the database
+          $query = "INSERT INTO users (username, email, password) 
+                    VALUES('$username', '$email', '$password')";
+          mysqli_query($db, $query);
+    
+          $_SESSION['username'] = $username;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: login.php');
+       
+        }
+      }
+  
 
-		// register user if there are no errors in the form
-		if (count($errors) == 0) {
-			$password = md5($password);//encrypt the password before saving in the database
-			$query = "INSERT INTO users (username, email, password) 
-					  VALUES('$username', '$email', '$password')";
-			mysqli_query($db, $query);
+		
 
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "You are now logged in";
-			header('location: login.php');
-		}
+		
+		
 
 	}
 
