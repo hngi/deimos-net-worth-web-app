1c718a2fda6bee522836de627b5342779843e536
Ovie Tennyson <atomictenny@gmail.com>
2019-09-26 10:48:50 -0700

    Feedback Modification

--- error.php
+++ error.php
@@ -1,8 +1,8 @@
 <?php if(isset($_SESSION['error']) ): $errors = $_SESSION['error'];  ?>
 <div class="alert alert-danger">
-<h5>    
+<span style="font-size:13px; font-weight:bold;">    
     <span><?php echo $errors[0]; ?> </span> 
-</h5>
+</span>
 </div>
 <?php endif; ?>
 
