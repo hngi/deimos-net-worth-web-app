ec8b5190860931fdfd6d67e70b9e9bbcbff5614a
Ovie Tennyson <atomictenny@gmail.com>
2019-10-05 15:04:51 -0700

    bug fixes

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ session_start();
 
 	// connect to database
 
-    $db = mysqli_connect('localhost', 'root', '12345678', 'deimos');
+    $db = mysqli_connect('localhost', 'root', '12345678', 'registration');
 
     
     // LOGIN USER
