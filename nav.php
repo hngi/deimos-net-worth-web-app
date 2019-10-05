520dbe728a26aee9da129790efb6b752e9eea799
Ovie Tennyson <atomictenny@gmail.com>
2019-10-05 15:08:41 -0700

    UI changes

--- nav.php
+++ nav.php
@@ -12,6 +12,30 @@
 <li class="nav-item">
     <span class="m-3 pt-1 pl-3 pr-3 pb-1" style="font-weight:bold; color:#fff !important; ">Hello, <?php echo ucfirst($username);?> &nbsp;</span>  
 </li>
+
+<li class="nav-item">
+    <span class="m-3 pt-1 pl-3 pr-3 pb-1" style="font-weight:bold; ">
+        <a href="contact.php" style="text-decoration:none!important; color:#fff !important;">Contact Us &nbsp; |</a>
+    </span>
+</li>
+
+<li class="nav-item">
+    <span class="m-3 pt-1 pl-3 pr-3 pb-1" style="font-weight:bold; ">
+        <a href="faq.php" style="text-decoration:none!important; color:#fff !important;">FAQs&nbsp; |</a>
+    </span>
+</li>
+
+<!-- <li class="nav-item">
+    <form action="contact.php">
+        <button type="submit" name="contact"> &nbsp;Contact Us &nbsp;|</button>
+    </form>
+</li>
+
+<li class="nav-item">
+    <form action="faq.php">
+        <button   type="submit" name="faq">&nbsp;FAQs&nbsp;|</button>
+    </form>
+</li> -->
 <li class="nav-item">
     <form action="server.php">
         <button type="submit" class="m-3 pt-1 pl-3 pr-3 pb-1" name="logout"> &gt; &gt; &nbsp;Logout</button>
