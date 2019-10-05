ff4203cd1ee370482c7b8f11329a3d42b5be9afa
anthonynzube <nzubeanthony4@gmail.com>
2019-10-04 23:28:08 -0700

    Merged Francis's Contact form with investment page

--- investment.php
+++ investment.php
@@ -86,6 +86,12 @@ if (isset($_POST["send"])) {
                             <button type="submit">&nbsp;Return to Dashboard&nbsp;</button>
                         </form>
                     </li>&nbsp;&nbsp;
+
+                    <li class="nav-item">
+                        <form action="contact.php">
+                            <button type="submit">&nbsp;Contact Us&nbsp;</button>
+                        </form>
+                    </li>&nbsp;&nbsp;
                    
 
                     <li class="nav-item">
