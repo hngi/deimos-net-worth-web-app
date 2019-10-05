7b33a36f1acb550140c8565bdedcc2cca297dd22
luqmonac56 <luqmonac56@gmail.com>
2019-10-05 17:46:19 +0000

    about

---
+++ about.php
@@ -0,0 +1,119 @@
+<?php 
+session_start();
+// var_dump($_SESSION['username']); die();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="index.html" rel="canonical">
+    <link href="index.php" rel="canonical">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700,900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/36afc40636.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="icon" type="image/x-icon" href="./img/Purple logo Group.png">
+    <title>Deimos' Networth Calculator | About Us</title>
+</head>
+<body>
+        <header>
+                <nav class="navbar navbar-expand-lg navbar-light container-fluid" id="header__nav">
+                    <a class="navbar-brand" href="index.php"><img src="./img/networth logo.svg"><span class=" text-light header__nav__brand ml-3 mt-2 font-weight-bold">NetWorth</span></a>
+    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ml-auto navbar__menu">
+     
+                            <form action="index.html">
+    
+                            <form action="index.php">
+    
+                                <li class="nav-item active">
+                                    <button class="m-3 pt-1 pl-3 pr-3 pb-1">Home</button>
+                                </li>
+                            </form>
+    
+                            <form action="login.php">
+                                <li class="nav-item">
+    
+                                </li>
+                            </form>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+    
+
+    <main>
+             <div class="main">
+                 <div class="main1">
+                         <h2>Team Deimos Networth App</h2>
+                         <p class="project">Team Deimos is a group of developers,designers and digital marketers from HNG internship.</p>
+                 </div>
+             </div>
+            
+
+             <div class="about">
+                <div class="about-team">
+                    <h2>What we do in Team DEIMOS</h2>
+                    <ul>
+                        <li>Team Deimos is a group of developers,designers and digital marketers from HNG internship working together to solve real life problems.</li>
+                        <li> A networth app was recently built by the web developers, designers and digital marketers of the team and the app is being modified by the group members.</li>
+                        <li>A solar calculator was recently built by the mobile developers of the team and is currently undergoing modification by the team members.</li>
+                    </ul>
+                </div>
+                <div class="about-app">
+                    <h2>About the  App</h2>
+                    <ul>
+                        <li>The landing page is the first page seen by the user on openning the app.</li>
+                        <li>then a burger menu is placed at the top right corner of the landing page where we have the <strong>login</strong> and <strong>sign up</strong> button </li>
+                        <li>The user is expected to sign in and create an account.</li>
+                        <li>Then the dashbord will be displayed where the user is expected to input their assets and liabilities </li>
+                        <li>After which the networth of the user will be displayed. </li>
+                        <li>The user also have access to a free networth certificate after checking your networth with the Deimos networth app.</li>
+                    </ul>
+                </div>
+             </div>
+             <div>
+                 <div class="recent">
+                 <center class="headin">Recent Projects</center> 
+                 <div class="sekond">
+                     <img class="pic" src="img/10.PNG" alt="" width="200" height="350">
+                     <img class="pic" src="img/11.PNG" alt="" width="200" height="350">
+                     <img class="pic" src="img/12.PNG" alt="" width="200" height="350">
+                     <img class="pic" src="img/13.PNG" alt="" width="200" height="350">
+                     <img class="pic" src="img/14.PNG" alt="" width="200" height="350">
+                 </div>
+                 </div>
+             </div>
+             <div class="down">
+                 <div class="abuja">
+                     <h3>Abuja Address</h3>
+                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae cum excepturi facilis nesciunt adipisci ab, ipsum, numquam voluptatum ipsa sunt vitae distinctio dignissimos ratione neque dolorum quisquam dolores iure corrupti!</p>
+                 </div>
+                 <div class="lagos">
+                     <h3>Lagos Address</h3>
+                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam saepe deleniti ipsum dignissimos iusto, quos voluptates, nostrum dolor aperiam tempora dolorem tenetur assumenda. Minima veritatis ut hic dignissimos a nihil.</p>
+                 </div>
+                 <div class="contacts">
+                     <h3>Contacts</h3>
+                     <p>Lorem ipsum dolor sit,usdam autem asperiores in.</p>
+                 </div>
+             </div>
+    </main>
+ <script src="main.js"></script>
+ </body>
+</html>
+
+
+
+
+
+
